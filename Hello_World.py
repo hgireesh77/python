69e79b18206bfca625f65c6869f749edf858a0db
hgireesh77 <hgireesh77@gmail.com>
2026-06-11 21:43:13 +0530

    Update Hello_World.py

--- Hello_World.py
+++ Hello_World.py
@@ -3,6 +3,7 @@ name = (
     os.environ.get("SEMAPHORE_NAME") or 
     os.environ.get("name") or 
     os.environ.get("SEMAPHORE_SURVEY_NAME") or
+    os.environ.get("ANSIBLE_NAME") or
     "No Name Provided"
 )
 print(f"Hello World....{name}")
