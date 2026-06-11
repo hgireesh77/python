9f40658ad955ef6c97a08e73e56cb2c09fcfa93a
hgireesh77 <hgireesh77@gmail.com>
2026-06-11 21:37:34 +0530

    Update Hello_World.py

--- Hello_World.py
+++ Hello_World.py
@@ -1,4 +1,9 @@
 import os
 
-name = os.environ.get("name")
+name = (
+    os.environ.get("SEMAPHORE_NAME") or 
+    os.environ.get("name") or 
+    os.environ.get("SEMAPHORE_SURVEY_NAME") or
+    "No Name Provided"
+)
 print(f"Hello World....{name}")
